fb356de0145c0e30fe807f1a6b7288fa23043d5c
hunghuy1907 <hunghuy1907@gmail.com>
2019-04-07 02:49:13 +0700

    login facebook

--- app/src/main/java/com/hungth/cotoan/screen/home/HomeFragment.java
+++ app/src/main/java/com/hungth/cotoan/screen/home/HomeFragment.java
@@ -8,13 +8,16 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.hungth.cotoan.R;
@@ -28,11 +31,14 @@ import org.json.JSONObject;
 
 import java.util.Arrays;
 
-public class HomeFragment extends BaseFragment {
+import static com.facebook.FacebookSdk.getApplicationContext;
+
+public class HomeFragment extends BaseFragment{
     public static String TAG = HomeFragment.class.getSimpleName();
     private FragmentHomeBinding binding;
     private HomeViewModel viewModel;
     private CallbackManager callbackManager;
+    private LoginButton loginButton;
 
     @Nullable
     @Override
@@ -42,8 +48,16 @@ public class HomeFragment extends BaseFragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(getApplicationContext());
+    }
+
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         viewModel = new HomeViewModel(ChessManRepository.getInstance(ChessmanLocalDataSource.getInstance(getActivity())));
         binding.setViewModel(viewModel);
         initData();
@@ -54,37 +68,19 @@ public class HomeFragment extends BaseFragment {
     }
 
     public void initData() {
-        final LoginButton loginButton = binding.loginButton;
+        loginButton = binding.loginButton;
         callbackManager = CallbackManager.Factory.create();
         loginButton.setReadPermissions(Arrays.asList(
                 "public_profile", "email"));
+        loginButton.setFragment(this);
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(),
-                        new GraphRequest.GraphJSONObjectCallback() {
-                            @Override
-                            public void onCompleted(JSONObject object, GraphResponse response) {
-                                System.out.println("---->>>JSON: " + response.toString());
-                                try {
-                                    String email = object.getString("email");
-                                    String birthday = object.getString("birthday");
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-                loginButton.setVisibility(View.INVISIBLE);
-                System.out.println("---->>> success: ");
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,email");
-                graphRequest.setParameters(parameters);
-                graphRequest.executeAsync();
+                loginResult(loginResult);
             }
 
             @Override
             public void onCancel() {
-                System.out.println("---->>> cancle: ");
             }
 
             @Override
@@ -92,27 +88,48 @@ public class HomeFragment extends BaseFragment {
                 System.out.println("---->>> error: " + error);
             }
         });
+
+        binding.buttonPlayOnline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "My message to send");
+                sendIntent.setType("text/plain");
+                sendIntent.setPackage("com.facebook.orca");
+
+                try {
+                    startActivity(sendIntent);
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(getActivity(), "Please", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-//    public void loginResult() {
-//        GraphRequest graphRequest = GraphRequest.newMeRequest(binding.loginButton.getAccessToken(),
-//                new GraphRequest.GraphJSONObjectCallback() {
-//                    @Override
-//                    public void onCompleted(JSONObject object, GraphResponse response) {
-//                        System.out.println("---->>>JSON: " + response.getJSONObject().toString());
-//                        try {
-//                            String email = object.getString("email");
-//                            String birthday = object.getString("birthday");
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                });
-//        Bundle parameters = new Bundle();
-//        parameters.putString("fields", "id,name,email");
-//        graphRequest.setParameters(parameters);
-//        graphRequest.executeAsync();
-//    }
+    public void loginResult(LoginResult loginResult) {
+        GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        System.out.println("---->>>JSON: " + response.toString());
+                        try {
+                            String email = object.getString("email");
+                            String birthday = object.getString("birthday");
+                            String id = object.getString("id");
+                            System.out.println("");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        loginButton.setVisibility(View.INVISIBLE);
+        System.out.println("---->>> success: ");
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,email");
+        graphRequest.setParameters(parameters);
+        graphRequest.executeAsync();
+    }
 
 
     @Override
