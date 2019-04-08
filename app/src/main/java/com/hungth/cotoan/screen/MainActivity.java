8466b984153173e374acb4096446420d29cd69b2
hunghuy1907 <hunghuy1907@gmail.com>
2019-04-09 01:06:46 +0700

    sd

--- app/src/main/java/com/hungth/cotoan/screen/MainActivity.java
+++ app/src/main/java/com/hungth/cotoan/screen/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.hungth.cotoan.R;
 import com.hungth.cotoan.screen.home.HomeFragment;
@@ -20,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         FragmentTransactionUtils.addFragment(getSupportFragmentManager(),
                 HomeFragment.getInstance(),
@@ -59,7 +64,5 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .setNegativeButton("Không", null)
                 .show();
-
-
     }
 }
